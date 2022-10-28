ff5e61e9faad5256fe9b0c98c1838ccc1f7dc833
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-28 22:51:59 +0530

    added Finding Maximum in all using Generics Program

---
+++ MaximumInArrayGenerics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class MaximumInArrayGenerics<T> where T : IComparable
+    {
+        public T[] value;
+        public MaximumInArrayGenerics(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;          
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[sorted_values.Length -1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum Value is "+max);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            Console.WriteLine("Find Maximum Values");
+            int[] intArray = { 112, 344, 432, 555, 678 };
+            MaximumInArrayGenerics<int> genericsint = new MaximumInArrayGenerics<int>(intArray);
+            genericsint.PrintMaxValue();
+            float[] floatArray = { 1.2f, 34.4f, 4.32f, 55.5f, 6.78f };
+            MaximumInArrayGenerics<float> genericsfloat = new MaximumInArrayGenerics<float>(floatArray);
+            genericsfloat.PrintMaxValue();
+            string[] stringArray = { "Red", "Blue", "Orange", "Green", "Yellow" };
+            MaximumInArrayGenerics<string> genericsstring = new MaximumInArrayGenerics<string>(stringArray);
+            genericsstring.PrintMaxValue();
+
+        }
+    
+    }
+}
